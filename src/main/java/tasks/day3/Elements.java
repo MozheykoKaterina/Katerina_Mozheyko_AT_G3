c3a255f6960139918cfc9bb4ba64c511810caab0
MozheykoKaterina <katik010489@gmail.com>
2021-06-15 15:14:47 +0300

    added day3

---
+++ src/main/java/tasks/day3/Elements.java
@@ -0,0 +1,21 @@
+package src.main.java.tasks.day3;
+
+import java.util.Random;
+
+public class Elements {
+
+    public void elements() {
+        int[] array = new int[7];
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(20);
+        }
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+    }
+
+}
